d5cc90b604b914dcbaf92a21f1f94c3db9636b3b
Phil Xing <phil.xing@adlinktech.com>
2022-08-09 14:12:19 +0800

    Disable MMC version info. support for A1 board stepping.

--- Library/MmcLib/MmcLib.c
+++ Library/MmcLib/MmcLib.c
@@ -15,6 +15,7 @@
 #include <Library/PL011UartLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/MmcLib.h>
+#include <Library/NVLib.h>
 
 EFI_STATUS
 MmcPostCode(
@@ -69,6 +70,11 @@ MmcFirmwareVersion(
   UINT8 xBuffer[19 * 3 + 5 + 12];
   UINTN xBufferSize = sizeof(xBuffer);
 
+  if (GetFirmwareMajorVersion()==0xA1) {
+    DEBUG((DEBUG_INFO, "%a A1 is not supported\n", __FUNCTION__));
+    return EFI_UNSUPPORTED;
+  }
+
   NumberOfBytes = PL011UartWrite((UINTN)PcdGet64(PcdSerialDbgRegisterBase), IpmiCmdBuf, IpmiCmdBufSize);
 
   if (NumberOfBytes == 0)
@@ -87,17 +93,17 @@ MmcFirmwareVersion(
     return EFI_NO_RESPONSE;
   }
 
-  DEBUG((DEBUG_VERBOSE, "%a [18 00 01]--->", __FUNCTION__));
+  DEBUG((DEBUG_INFO, "%a [18 00 01]--->", __FUNCTION__));
   for (int i=0; i< xBufferSize; i++) {
-    DEBUG((DEBUG_VERBOSE, "%02d:%02x ", i, xBuffer[i]));
+    DEBUG((DEBUG_INFO, "%02d:%02x ", i, xBuffer[i]));
   }
-  DEBUG((DEBUG_VERBOSE, "\n"));
+  DEBUG((DEBUG_INFO, "\n"));
 
-  DEBUG((DEBUG_VERBOSE, "%a [18 00 01]--->", __FUNCTION__));
+  DEBUG((DEBUG_INFO, "%a [18 00 01]--->", __FUNCTION__));
   for (int i=0; i< xBufferSize; i++) {
-    DEBUG((DEBUG_VERBOSE, "%c", xBuffer[i]));
+    DEBUG((DEBUG_INFO, "%c", xBuffer[i]));
   }
-  DEBUG((DEBUG_VERBOSE, "\n"));
+  DEBUG((DEBUG_INFO, "\n"));
 
   CopyMem(Buffer, xBuffer + 66, BufferSize - 1);
 
