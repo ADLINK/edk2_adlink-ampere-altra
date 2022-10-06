4d2bcf16cab800f5a973723c068bc39dd20eb38e
Phil.Xing <phil.xing@adlinktech.com>
2022-10-06 16:34:22 +0800

    Update scripts.

--- tools/target_power.sh
+++ tools/target_power.sh
@@ -3,22 +3,22 @@
 
 # uncomment and modify to adapt environment for USB Relay HID
 #
-# if command -v usbrelay &> /dev/null; then
-#     if [ $1 == 'ON' ]; then
-#         usbrelay HW348_1=0
-#     else    
-#         usbrelay HW348_1=1
-#         sleep 3
-#     fi    
-# fi    
+if command -v usbrelay &> /dev/null; then
+    if [ $1 == 'ON' ]; then
+        usbrelay HW348_1=0
+    else
+        usbrelay HW348_1=1
+        sleep 3
+    fi
+fi
 
 # uncomment and modify to adapt environment for USB Relay CH340
 
-# if command -v adlink-platforms/toolchain/UsbRelayCh340 &> /dev/null; then
-#     if [ $1 == 'ON' ]; then
-#         adlink-platforms/toolchain/UsbRelayCh340 /dev/ttyUSB0 NC
-#     else    
-#         adlink-platforms/toolchain/UsbRelayCh340 /dev/ttyUSB0 NO
-#         sleep 3
-#     fi    
-# fi    
+if command -v UsbRelayCh340 &> /dev/null; then
+    if [ $1 == 'ON' ]; then
+        UsbRelayCh340 /dev/ttyUSB0 NC
+    else
+        UsbRelayCh340 /dev/ttyUSB0 NO
+        sleep 3
+    fi
+fi
