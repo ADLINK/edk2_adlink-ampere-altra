2ec18541a476e2a57303a736586aa3f257e736b1
Phil.Xing <phil.xing@adlinktech.com>
2022-11-03 10:28:32 +0800

    Update scripts.

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OPATH=$PATH
 export WORKSPACE=$PWD
-. $WORKSPACE/edk2.sh
+. $WORKSPACE/edk2_adlink-ampere-altra/tools/edk2.sh
 make -f $WORKSPACE/edk2-ampere-tools/Makefile clean
 export PATH=$OPATH
